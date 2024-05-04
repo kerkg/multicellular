29367287fd242a121eef7d6f5144758366afd223
kerkg <keremkg2007@gmail.com>
2024-05-05 00:18:16 +0300

    decoder works but too slow, I probably thread a bit but, ı dunno it might be python havin internal problems but also gotta sleep so bye?

--- experiments/decos.py
+++ experiments/decos.py
@@ -1,31 +1,5 @@
 import gc
-
-
-class SubSet:
-    def __init__(self, iterable=None):
-        self.items = set()
-        if iterable:
-            self.items.update(iterable)
-
-    def add(self, item):
-        self.items.add(item)
-
-    def remove(self, item):
-        self.items.remove(item)
-
-    def __contains__(self, item):
-        return item in self.items
-
-    def __str__(self):
-        return str(self.items)
-
-
-# TODO:  cellworks
-# -META-
-# pargf
-# N>(0,0),(0,0,0)<
-# apr_s>4750052<
-# -DATA-
+# TODO:  cell-works
 
 
 def pargf_builder(file: str, natural=((0, 0), (0, 0, 0)), number_of_cells: int = 250000):
@@ -44,20 +18,19 @@ def pargf_builder(file: str, natural=((0, 0), (0, 0, 0)), number_of_cells: int =
         file.write(str(screen_list))
     return 1
 
+
 def progress_finder(whole_task: int, done_task: int, progress_note: str = None) -> tuple:
     return whole_task / done_task, progress_note
 
 
-def pargf_decoder(p, file_name, file) -> set:
+def pargf_decoder(p, file_name, file, auto_log) -> set:
     """
     :param file_name: name of the file
     :param file: raw file
     :param p: pointer
     :return: the execution queue( called ex_queue )
     """
-    # creating vars for metadata decoding
-    file_raw_obj_data = ""
-    apr_size = ""  # stands for approximate_size
+    if auto_log: print("debugging started( oh boi, here it comes")
     # "natural" decoder
     natural = ""
     p += 1
@@ -65,35 +38,42 @@ def pargf_decoder(p, file_name, file) -> set:
         natural += file[p]
         p += 1
     exec(f"natural = {natural}")
+    if auto_log: print("natural decoded")
+    # apr_char_num decoder
     p += 8
-    # apr_size decoder
+    apr_char_num = ""  # stands for approximate_size
     while file[p] != "<":
-        apr_size += file[p]
+        apr_char_num += file[p]
         p += 1
-    apr_size = int(apr_size)
+    apr_char_num = int(apr_char_num)
+    if auto_log: print("apr_char_num decoded")
+    # decoding raw file data to the raw_page_obj
     p += 10
-    nsvr = ""  # stands for numerical_SubValue_Register
-    cell_register = [(), ()]
+    raw_page_obj = []
+    cell_cache = ""
+    subcell_value_end_counter = 0
+    num_of_cells = 0
+    if auto_log: print("pre-declaring the vars")
     while True:
-        while file[p] != ")":
-            if file[p] != " ":
-                while file[p] != ",":
-                    nsvr += file[p]
-                if nsvr.isnumeric():
-                    nsvr = int(nsvr)
-                cell_register[0] = nsvr
-            else:
-                p += 1
-        #         decoder partially works
+        cell_cache += file[p]
+        if file[p] == ")":
+            print(p, subcell_value_end_counter)
+            subcell_value_end_counter += 1
+            if subcell_value_end_counter == 2:
+                print(f"on exec, {num_of_cells}")
+                exec(f"cell_cache={cell_cache}")
+                raw_page_obj.append(cell_cache)
+                subcell_value_end_counter = 0
+                num_of_cells += 1
+                if num_of_cells % 10000 == 0 and auto_log:
+                    print(f"10k cell has processed, p is:{p}")
         if file[p] == "]":
             file_len = p + 1
             break
         p += 1
-    # decoding the obj_data
-
-    del file
+    del file, subcell_value_end_counter, p, cell_cache
     gc.collect()
-    return {file_name, apr_size, file_len, natural, 1}
+    return {file_name, apr_char_num, file_len, natural, num_of_cells, raw_page_obj}
 
 
 def argf_decoder(file, p):
@@ -102,23 +82,33 @@ def argf_decoder(file, p):
     NotImplementedError("work in progress")
 
 
-def file_directer(file_name: str) -> None:
+def file_directer(file_name: str, auto_log):
     """
+    :param auto_log: if set to true will log the activities, usefull for debugging and looks cool( I mean who doesn't
+    get excited when the terminal pops up and starts doing something, eh)
     :param file_name: name of the file with suffix
     fetches the files from the disk and directs the screen data to the decoders
     """
     # fetching the data from the disk
+    if auto_log: print(f"fetching the {file_name} from the disk")
     with open(file_name, "r") as file:
         file = file.read()
+    if auto_log: print("fetching successful, invoking the decoding")
     file_name = file_name.strip(".txt")
     p = 0  # stands for pointer
     p += 8
     if file[p] == "a":  # the letter "a" is the second letter in the word "pargf" btw
         p += 6
-        pargf_decoder(p, file_name, file)
+        if auto_log: print("decoding")
+        res = pargf_decoder(p, file_name, file, auto_log=auto_log)
     elif file[p] == "r":  # the letter "r" is the second letter in the word "argf" btw
         p += 5
+        if auto_log: print("decoding")
         argf_decoder(file, p)
     else:
         # make the compiler
         p += 5
+    return res
+
+
+file_directer("texture.txt", True)
